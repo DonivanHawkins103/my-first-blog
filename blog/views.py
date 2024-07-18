d81f731ae5978eba02be6e06e53c7646dd78a1fb
DonivanHawkins103 <150474492+DonivanHawkins103@users.noreply.github.com>
2024-07-18 13:20:29 -0500

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -6,6 +6,12 @@ from .forms import PostForm
 
 Post.objects.get(pk=pk)
 
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method=='POST':
+        post.publish()
+    return redirect('post_detail', pk=pk)
+
 def post_draft_list(request):
     posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
     return render(request, 'blog/post_draft_list.html', {'posts': posts})
